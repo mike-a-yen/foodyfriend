ff6ec286b25875694a2e0bf8b9d468afe96f8e5b
mike-a-yen <mike.a.yen@gmail.com>
2016-09-25 15:21:30 -0700

    db models

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from . import db
+
+db.create_all()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(120), unique=True)
+    
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        
+    def __repr__(self):
+        return '<Name %r>' % self.name
