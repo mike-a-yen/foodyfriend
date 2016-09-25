1a21b878809305a789538e6a216d6a3ed4189cd7
mike-a-yen <mike.a.yen@gmail.com>
2016-09-25 15:14:17 -0700

    restructured config

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 import os
 from app import views, utils
-from app.config import settings
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
