e342482daf93850df1396539736c23d82473c71b
mike-a-yen <mike.a.yen@gmail.com>
2016-10-09 21:43:23 -0700

    split views

---
+++ app/user_views.py
@@ -0,0 +1,86 @@
+from app import app, db, login_manager
+import flask
+from flask_login import login_user, login_required, logout_user
+import flask_login
+from datetime import datetime
+from .models import User, Review
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return flask.render_template('index.html')
+
+@app.context_processor
+def inject_user():
+    return dict(user=flask_login.current_user)
+
+@app.route('/users')
+def users():
+    table = User.query.all()
+    print(table)
+    return flask.render_template("table.html", table=table)
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if flask.request.method == 'GET':
+        return flask.render_template('register.html')
+    user = User(flask.request.form['email'],
+                flask.request.form['password'])
+    print(user)
+    db.session.add(user)
+    db.session.commit()
+    flask.flash('Registration Successfully Completed!')
+    return flask.redirect('/login')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    current_user = flask_login.current_user
+    if flask.request.method == 'GET':
+        return flask.render_template('login.html',current_user=current_user)
+    email = flask.request.form['email']
+    password = flask.request.form['password']
+    registered_user = User.query.filter_by(email=email).first()
+    if registered_user == None or not registered_user.check_password(password):
+        flask.flash('email or password invalid', 'error')
+        return flask.redirect('/login')
+    if registered_user.check_password(password):
+        login_user(registered_user)
+        flask.flash('Logged in successfully')
+    return flask.redirect(flask.request.args.get('next') or flask.url_for('index'))
+
+@app.route('/profile')
+@login_required
+def profile():
+    user = flask_login.current_user
+    print(user)
+    return flask.render_template('profile.html', current_user=user)
+
+@app.route('/edit_profile',methods=['GET','POST'])
+@login_required
+def edit_profile():
+    current_user = flask_login.current_user
+    print(current_user.id)
+    print(current_user.email)
+    print(current_user.name)
+    if flask.request.method == 'GET':
+        return flask.render_template('edit_profile.html',current_user=current_user)
+    email = flask.request.form.get('email',current_user.email)
+    password = flask.request.form.get('password',current_user.password)
+    name = flask.request.form.get('name',current_user.name)
+
+    user = User.query.filter_by(id=current_user.id).first()
+    user.name = name
+    user.email = email
+    user.password = password
+    db.session.commit()
+    return flask.redirect(flask.request.args.get('next') or flask.url_for('profile'))
+    
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return flask.redirect('/login')
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
